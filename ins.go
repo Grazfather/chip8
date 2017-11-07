0e155377c201f5a825ef5470e28b69d9568d1265
Grazfather <grazfather@gmail.com>
2017-11-06 22:36:46 -0800

    Add more doc strings to opcodes

--- ins.go
+++ ins.go
@@ -4,29 +4,30 @@ import (
 	"math/rand"
 )
 
-// Opcode0NNN is an ignored opcode
+// Opcode0NNN is an ignored opcode.
 func (c *Chip8) Opcode0NNN(ins uint16) {
 	panic("unimplemented")
 }
 
-// Opcode00E0 clears the screen
+// Opcode00E0 clears the screen.
 func (c *Chip8) Opcode00E0(ins uint16) {
 	c.screen.OnEachPixel(ClearPixel)
 }
 
+// Opcode00EE returns from a subroutine.
 func (c *Chip8) Opcode00EE(ins uint16) {
 	// TODO: The RA is actually the call address, which will get incremented
 	c.pc = c.stack[c.sp]
 	c.sp -= 2
 }
 
-// Opcode1NNN jumps to address NNN
+// Opcode1NNN jumps to address NNN.
 func (c *Chip8) Opcode1NNN(ins uint16) {
 	// TODO: PC target again. Compensating here with -2
 	c.pc = ArgNNN(ins) - 2
 }
 
-// Opcode2NNN calls the subroutne at NNN
+// Opcode2NNN calls the subroutne at NNN.
 func (c *Chip8) Opcode2NNN(ins uint16) {
 	// TODO: set PC target. For now we count on the loop incrementing past the call instruction
 	c.sp += 2
@@ -34,49 +35,58 @@ func (c *Chip8) Opcode2NNN(ins uint16) {
 	c.pc = ArgNNN(ins) - 2
 }
 
-// Opcode3XNN Skips the next instruction if Vx equals NN
+// Opcode3XNN skips the next instruction if Vx equals NN.
 func (c *Chip8) Opcode3XNN(ins uint16) {
 	if c.v[ArgX(ins)] == ArgNN(ins) {
 		c.pc += 2
 	}
 }
 
+// Opcode3XNN skips the next instruction if Vx does not equal NN.
 func (c *Chip8) Opcode4XNN(ins uint16) {
 	if c.v[ArgX(ins)] != ArgNN(ins) {
 		c.pc += 2
 	}
 }
 
+// Opcode3XNN skips the next instruction if Vx equals Vy.
 func (c *Chip8) Opcode5XY0(ins uint16) {
 	if c.v[ArgX(ins)] == c.v[ArgY(ins)] {
 		c.pc += 2
 	}
 }
 
+// Opcode6XNN sets Vx to NN.
 func (c *Chip8) Opcode6XNN(ins uint16) {
 	c.v[ArgX(ins)] = ArgNN(ins)
 }
 
+// Opcode7XNN add NN to Vx.
 func (c *Chip8) Opcode7XNN(ins uint16) {
 	c.v[ArgX(ins)] += ArgNN(ins)
 }
 
+// Opcode8XY0 sets Vx to the value of Vy.
 func (c *Chip8) Opcode8XY0(ins uint16) {
 	c.v[ArgX(ins)] = c.v[ArgY(ins)]
 }
 
+// Opcode8XY1 sets Vx to Vx | Vy.
 func (c *Chip8) Opcode8XY1(ins uint16) {
 	c.v[ArgX(ins)] |= c.v[ArgY(ins)]
 }
 
+// Opcode8XY2 sets Vx to Vx & Vy.
 func (c *Chip8) Opcode8XY2(ins uint16) {
 	c.v[ArgX(ins)] &= c.v[ArgY(ins)]
 }
 
+// Opcode8XY3 sets Vx to Vx ^ Vy.
 func (c *Chip8) Opcode8XY3(ins uint16) {
 	c.v[ArgX(ins)] ^= c.v[ArgY(ins)]
 }
 
+// Opcode8XY4 adds Vy to Vx and sets VF to 1 when there's a carry.
 func (c *Chip8) Opcode8XY4(ins uint16) {
 	old := c.v[ArgX(ins)]
 	c.v[ArgX(ins)] += c.v[ArgY(ins)]
@@ -88,6 +98,7 @@ func (c *Chip8) Opcode8XY4(ins uint16) {
 	}
 }
 
+// Opcode8XY5 subtracts Vy from Vx and sets VF to 0 when there's a borrow.
 func (c *Chip8) Opcode8XY5(ins uint16) {
 	old := c.v[ArgX(ins)]
 	c.v[ArgX(ins)] -= c.v[ArgY(ins)]
@@ -99,6 +110,8 @@ func (c *Chip8) Opcode8XY5(ins uint16) {
 	}
 }
 
+// Opcode8XY6 shifts Vy right by one and copies the result to Vx. VF is set to
+// the value of the least significant bit of Vy before the shift.
 func (c *Chip8) Opcode8XY6(ins uint16) {
 	lsb := c.v[ArgY(ins)] & 1
 	v := c.v[ArgY(ins)] >> 1
@@ -107,6 +120,7 @@ func (c *Chip8) Opcode8XY6(ins uint16) {
 	c.v[VF] = lsb
 }
 
+// Opcode8XY7 sets Vx to Vy minus Vx and sets VF when there's a borrow.
 func (c *Chip8) Opcode8XY7(ins uint16) {
 	y := c.v[ArgY(ins)]
 	x := c.v[ArgX(ins)]
@@ -119,35 +133,39 @@ func (c *Chip8) Opcode8XY7(ins uint16) {
 	}
 }
 
+// Opcode8XYE shifts Vy left by one and copies the result to Vx. VF is set to the value of the most significant bit of Vy before the shift.
 func (c *Chip8) Opcode8XYE(ins uint16) {
-	msg := c.v[ArgY(ins)] >> 7 & 1
+	msb := c.v[ArgY(ins)] >> 7 & 1
 	v := c.v[ArgY(ins)] << 1
 	c.v[ArgX(ins)] = v
 	c.v[ArgY(ins)] = v
-	c.v[VF] = msg
+	c.v[VF] = msb
 }
 
+// Opcode9XY0 skips the next instruction if Vx doesn't equal Vy.
 func (c *Chip8) Opcode9XY0(ins uint16) {
 	if c.v[ArgX(ins)] != c.v[ArgY(ins)] {
 		c.pc += 2
 	}
 }
 
+// OpcodeANNN sets I to the address NNN.
 func (c *Chip8) OpcodeANNN(ins uint16) {
 	c.i = ArgNNN(ins)
 }
 
+// OpcodeBNNN Jumps to dhe address NNN  plus V0.
 func (c *Chip8) OpcodeBNNN(ins uint16) {
 	// TODO: Want to skip the +=2 at the end of the loop
 	c.pc = (uint16(c.v[V0]) + ArgNNN(ins) - 2) & 0xFFF
 }
 
-// OpcodeCXNN sets Vx to the result of rand()&NN
+// OpcodeCXNN sets Vx to the result of rand()&NN.
 func (c *Chip8) OpcodeCXNN(ins uint16) {
 	c.v[ArgX(ins)] = uint8(rand.Uint32()) & ArgNN(ins)
 }
 
-// OpcodeDXYN draws a sprite I to Vx, Vy with width 8 height N
+// OpcodeDXYN draws a sprite I to Vx, Vy with width 8 height N.
 func (c *Chip8) OpcodeDXYN(ins uint16) {
 	x := c.v[ArgX(ins)]
 	y := c.v[ArgY(ins)]
@@ -174,51 +192,51 @@ func (c *Chip8) OpcodeDXYN(ins uint16) {
 	c.Render(c.screen)
 }
 
-// OpcodeEX9E skips the next instruction if key Vx is pressed
+// OpcodeEX9E skips the next instruction if key Vx is pressed.
 func (c *Chip8) OpcodeEX9E(ins uint16) {
 	if c.keypad.Pressed(c.v[ArgX(ins)]) {
 		c.pc += 2
 	}
 }
 
-// OpcodeEXA1 skips the next instruction if key Vx is not pressed
+// OpcodeEXA1 skips the next instruction if key Vx is not pressed.
 func (c *Chip8) OpcodeEXA1(ins uint16) {
 	if !c.keypad.Pressed(c.v[ArgX(ins)]) {
 		c.pc += 2
 	}
 }
 
-// OpcodeFX07 sets Vx to the value of the delay timer
+// OpcodeFX07 sets Vx to the value of the delay timer.
 func (c *Chip8) OpcodeFX07(ins uint16) {
 	c.v[ArgX(ins)] = c.delay
 }
 
-// OpcodeFX0A halts until a key is pressed, and stores it in Vx
+// OpcodeFX0A halts until a key is pressed, and stores it in Vx.
 func (c *Chip8) OpcodeFX0A(ins uint16) {
 	c.v[ArgX(ins)] = c.keypad.WaitPress()
 }
 
-// OpcodeFX15 sets the delay timer to Vx
+// OpcodeFX15 sets the delay timer to Vx.
 func (c *Chip8) OpcodeFX15(ins uint16) {
 	c.delay = c.v[ArgX(ins)]
 }
 
-// OpcodeFX18 sets the sound timer to Vx
+// OpcodeFX18 sets the sound timer to Vx.
 func (c *Chip8) OpcodeFX18(ins uint16) {
 	c.sound = c.v[ArgX(ins)]
 }
 
-// OpcodeFX1E adds Vx to I
+// OpcodeFX1E adds Vx to I.
 func (c *Chip8) OpcodeFX1E(ins uint16) {
 	c.i = (c.i + uint16(c.v[ArgX(ins)])) & 0xFFF
 }
 
-// OpcodeFX29 sets I to point to sprite for digit Vx
+// OpcodeFX29 sets I to point to sprite for digit Vx.
 func (c *Chip8) OpcodeFX29(ins uint16) {
 	c.i = 5 * uint16(c.v[ArgX(ins)])
 }
 
-// OpcodeFX33 stores the BCD representation of Vx into memory at I
+// OpcodeFX33 stores the BCD representation of Vx into memory at I.
 func (c *Chip8) OpcodeFX33(ins uint16) {
 	v := c.v[ArgX(ins)]
 	c.mem[c.i+2] = v % 10
@@ -228,7 +246,7 @@ func (c *Chip8) OpcodeFX33(ins uint16) {
 	c.mem[c.i] = v % 10
 }
 
-// OpcodeFX55 Stores V[0-X] inclusive in memory starting at address I
+// OpcodeFX55 Stores V[0-X] inclusive in memory starting at address I.
 func (c *Chip8) OpcodeFX55(ins uint16) {
 	x := ArgX(ins)
 	for r := uint8(0); r < x; r++ {
@@ -237,7 +255,7 @@ func (c *Chip8) OpcodeFX55(ins uint16) {
 	}
 }
 
-// OpcodeFX65 Loads V[0-X] inclusive from memory starting at address I
+// OpcodeFX65 Loads V[0-X] inclusive from memory starting at address I.
 func (c *Chip8) OpcodeFX65(ins uint16) {
 	x := ArgX(ins)
 	for r := uint8(0); r < x; r++ {
