1e15a05f73a070dae064090a9bbb16373384edfa
Grazfather <grazfather@gmail.com>
2017-11-20 20:44:54 -0800

    Use real mnemonics in disassembler

--- dis.go
+++ dis.go
@@ -30,96 +30,96 @@ func (d *Disassembler) dis(mem []byte) instruction {
 	case 0x0:
 		switch ins {
 		case 0x00E0:
-			return instruction{"Clear display", nil, ins}
+			return instruction{"CLS", nil, ins}
 		case 0x00EE:
-			return instruction{"Return", nil, ins}
+			return instruction{"RET", nil, ins}
 		default:
-			return instruction{"Call RCA 1802 %s", []string{SArgNNN(ins)}, ins}
+			return instruction{"SYS %s", []string{SArgNNN(ins)}, ins}
 		}
 	case 0x1:
-		return instruction{"Jump to %s", []string{SArgNNN(ins)}, ins}
+		return instruction{"JP %s", []string{SArgNNN(ins)}, ins}
 	case 0x2:
-		return instruction{"Call %s", []string{SArgNNN(ins)}, ins}
+		return instruction{"CALL %s", []string{SArgNNN(ins)}, ins}
 	case 0x3:
-		return instruction{"Skip next if %s == %s", []string{SArgX(ins), SArgNN(ins)}, ins}
+		return instruction{"SE %s, %s", []string{SArgX(ins), SArgNN(ins)}, ins}
 	case 0x4:
-		return instruction{"Skip next if %s != %s", []string{SArgX(ins), SArgNN(ins)}, ins}
+		return instruction{"SNE %s, %s", []string{SArgX(ins), SArgNN(ins)}, ins}
 	case 0x5:
-		return instruction{"Skip next if %s == %s", []string{SArgX(ins), SArgY(ins)}, ins}
+		return instruction{"SE %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 	case 0x6:
-		return instruction{"Set %s = %s", []string{SArgX(ins), SArgNN(ins)}, ins}
+		return instruction{"LD %s, %s", []string{SArgX(ins), SArgNN(ins)}, ins}
 	case 0x7:
-		return instruction{"Set %s += %s", []string{SArgX(ins), SArgNN(ins)}, ins}
+		return instruction{"ADD %s, %s", []string{SArgX(ins), SArgNN(ins)}, ins}
 	case 0x8:
 		switch ins & 0xF {
 		case 0x0:
-			return instruction{"Set %s = %s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"LD %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x1:
-			return instruction{"Set %s = %[1]s|%[2]s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"OR %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x2:
-			return instruction{"Set %s = %[1]s&%[2]s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"AND %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x3:
-			return instruction{"Set %s = %[1]s^%[2]s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"XOR %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x4:
-			return instruction{"Set %s += %s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"ADD %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x5:
-			return instruction{"Set %s -= %s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"SUB %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x6:
-			return instruction{"Set %s = %[2]s = %[1]s>>1", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"SHR %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0x7:
-			return instruction{"Set %s = %[2]s - %[1]s", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"SUBN %s %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		case 0xE:
-			return instruction{"Set %s = %[2]s = %[1]s<<1", []string{SArgX(ins), SArgY(ins)}, ins}
+			return instruction{"SHL %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 		default:
-			return instruction{"ILLEGAL", nil, ins}
+			return instruction{"<ILL>", nil, ins}
 		}
 	case 0x9:
 		if ins&0xF != 0 {
-			return instruction{"ILLEGAL", nil, ins}
+			return instruction{"<ILL>", nil, ins}
 		}
-		return instruction{"Skip next if %s != %s", []string{SArgX(ins), SArgY(ins)}, ins}
+		return instruction{"SNE %s, %s", []string{SArgX(ins), SArgY(ins)}, ins}
 	case 0xA:
-		return instruction{"Set I = %s", []string{SArgNNN(ins)}, ins}
+		return instruction{"LD I, %s", []string{SArgNNN(ins)}, ins}
 	case 0xB:
-		return instruction{"Jump to V0 + %s", []string{SArgNNN(ins)}, ins}
+		return instruction{"JP V0, ADDR", []string{SArgNNN(ins)}, ins}
 	case 0xC:
-		return instruction{"Set %s = rand() & %s", []string{SArgX(ins), SArgNN(ins)}, ins}
+		return instruction{"RND, %s, %s", []string{SArgX(ins), SArgNN(ins)}, ins}
 	case 0xD:
-		return instruction{"Draw sprite at I at (%s, %s) height %s", []string{SArgX(ins), SArgY(ins), SArgN(ins)}, ins}
+		return instruction{"DRW %s, %s, %s", []string{SArgX(ins), SArgY(ins), SArgN(ins)}, ins}
 	case 0xE:
 		switch ins & 0xFF {
 		case 0x9E:
-			return instruction{"Skip next if key() == %s", []string{SArgX(ins)}, ins}
+			return instruction{"SKP %s", []string{SArgX(ins)}, ins}
 		case 0xA1:
-			return instruction{"Skip next if key() != %s", []string{SArgX(ins)}, ins}
+			return instruction{"SKNP %s", []string{SArgX(ins)}, ins}
 		default:
-			return instruction{"ILLEGAL", nil, ins}
+			return instruction{"<ILL>", nil, ins}
 		}
 	case 0xF:
 		switch ins & 0xFF {
 		case 0x07:
-			return instruction{"Set %s = get_delay()", []string{SArgX(ins)}, ins}
+			return instruction{"LD %s, DT", []string{SArgX(ins)}, ins}
 		case 0x0A:
-			return instruction{"Set %s = get_key()", []string{SArgX(ins)}, ins}
+			return instruction{"LD %s, K", []string{SArgX(ins)}, ins}
 		case 0x15:
-			return instruction{"Set delay_timer = %s", []string{SArgX(ins)}, ins}
+			return instruction{"LD DT, %s", []string{SArgX(ins)}, ins}
 		case 0x18:
-			return instruction{"Set sound_timer = %s", []string{SArgX(ins)}, ins}
+			return instruction{"LD ST, %s", []string{SArgX(ins)}, ins}
 		case 0x1E:
-			return instruction{"Set I += %s", []string{SArgX(ins)}, ins}
+			return instruction{"ADD I, %s", []string{SArgX(ins)}, ins}
 		case 0x29:
-			return instruction{"Set I = sprite_addr[%s]", []string{SArgX(ins)}, ins}
+			return instruction{"LD F, %s", []string{SArgX(ins)}, ins}
 		case 0x33:
-			return instruction{"Store BCD(%s) at I", []string{SArgX(ins)}, ins}
+			return instruction{"LD B, %s", []string{SArgX(ins)}, ins}
 		case 0x55:
-			return instruction{"Store V0 to %s at I", []string{SArgX(ins)}, ins}
+			return instruction{"LD [I], %s", []string{SArgX(ins)}, ins}
 		case 0x65:
-			return instruction{"Load V0 to %s from I", []string{SArgX(ins)}, ins}
+			return instruction{"LD %s, [I]", []string{SArgX(ins)}, ins}
 		default:
-			return instruction{"ILLEGAL", nil, ins}
+			return instruction{"<ILL>", nil, ins}
 		}
 	default:
-		return instruction{"ILLEGAL", nil, ins}
+		return instruction{"<ILL>", nil, ins}
 	}
 }
 
