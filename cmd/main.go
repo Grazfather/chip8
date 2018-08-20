9ce73a6990531ae6cdaeef918bc194aced1e9c26
Grazfather <grazfather@gmail.com>
2018-08-19 21:55:11 -0700

    Use gocui display+keypad in main cmd

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"os/signal"
 	"time"
 
-	"github.com/nsf/termbox-go"
+	"github.com/jroimartin/gocui"
 
 	"github.com/Grazfather/chip8"
 )
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	var err error
+	if maxY < chip8.SCREEN_HEIGHT || maxX < chip8.SCREEN_WIDTH {
+		return fmt.Errorf("Cannot display if less than %d x %d! Resize your terminal! (^Q to quit)",
+			chip8.SCREEN_WIDTH, chip8.SCREEN_HEIGHT)
+	}
+	left := (maxX - chip8.SCREEN_WIDTH) / 2
+	_, err = g.SetView("display", left, 0, chip8.SCREEN_WIDTH+2+left, chip8.SCREEN_HEIGHT+2)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -17,17 +32,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	d, err := chip8.NewTerminal()
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		fmt.Printf("Error setting up renderer: %v\n", err)
-		os.Exit(1)
+		log.Panicln(err)
 	}
-	defer termbox.Close()
+	defer g.Close()
 
-	quit := make(chan termbox.Event)
-	k := chip8.NewTermKeypad(quit)
+	g.SetManagerFunc(layout)
+	// HACK: Need to call layout once to create the views
+	layout(g)
+	v, err := g.View("display")
+	if err != nil {
+		log.Panicln(err)
+	}
 
-	c := chip8.NewChip8(d, k)
+	g.SetCurrentView(v.Name())
+	k := chip8.NewGocuiKeypad(g, v)
+	r := chip8.NewGocuiRenderer(v)
+	c := chip8.NewChip8(r, k)
 	c.Reset()
 
 	if err := c.LoadBinary(os.Args[1]); err != nil {
@@ -35,28 +57,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	go c.KeepTime()
-
-	tick := time.Tick(2 * time.Millisecond)
-	exit := make(chan os.Signal)
-	signal.Notify(exit, os.Interrupt)
-
-LOOP:
-	for {
-		select {
-		case <-tick:
-			err := c.RunOne()
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				break LOOP
-			}
-		case <-c.Renderch:
-			c.Render()
-		case <-exit:
-			break LOOP
-		case <-quit:
-			break LOOP
-		}
+	if err := g.SetKeybinding("", gocui.KeyCtrlQ, gocui.ModNone,
+		func(g *gocui.Gui, v *gocui.View) error { return gocui.ErrQuit }); err != nil {
+		log.Panicln(err)
 	}
-	time.Sleep(1 * time.Second)
+
+	go func() {
+		go c.KeepTime()
+
+		tick := time.Tick(2 * time.Millisecond)
+	LOOP:
+		for {
+			select {
+			case <-tick:
+				err := c.RunOne()
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					break LOOP
+				}
+			case <-c.Renderch:
+				g.Update(func(g *gocui.Gui) error {
+					c.Render()
+					return nil
+				})
+			}
+		}
+	}()
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
 }
--- debugger.go
+++ debugger.go
@@ -24,7 +24,6 @@ var white = color.New(color.FgWhite, color.Bold).SprintFunc()
 var stop bool
 var stopped bool
 var first bool
-var tick = time.Tick(2 * time.Millisecond)
 
 func parseAddr(s string) (uint16, error) {
 	addr, err := strconv.ParseUint(s, 0, 16)
@@ -51,92 +50,6 @@ type ui struct {
 	displayView *gocui.View
 	debugView   *gocui.View
 	promptView  *gocui.View
-}
-
-type gocuiKeypad struct {
-	state       [16]bool
-	keyMap      map[rune]byte
-	wantnext    bool
-	key         chan byte
-	keyUpTimers map[rune]*time.Timer
-}
-
-func NewGocuiKeypad(g *gocui.Gui, view *gocui.View) *gocuiKeypad {
-	k := &gocuiKeypad{
-		keyMap: map[rune]byte{
-			'1': 0x1,
-			'2': 0x2,
-			'3': 0x3,
-			'4': 0xC,
-			'q': 0x4,
-			'w': 0x5,
-			'e': 0x6,
-			'r': 0xD,
-			'a': 0x7,
-			's': 0x8,
-			'd': 0x9,
-			'f': 0xE,
-			'z': 0xA,
-			'x': 0x0,
-			'c': 0xB,
-			'v': 0xF,
-		},
-	}
-
-	k.key = make(chan uint8)
-	k.keyUpTimers = make(map[rune]*time.Timer)
-
-	for key, code := range k.keyMap {
-		key := key
-		c := code
-		k.keyUpTimers[key] = time.AfterFunc(0, func() {
-			k.state[c] = false
-		})
-		g.SetKeybinding("display", key, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
-			// HACK: reset the timer for that key. On expiration mark as up.
-			k.keyUpTimers[key].Reset(100 * time.Millisecond)
-			k.state[c] = true
-			// If the emulator is in WaitPress then send it the key
-			if k.wantnext == true {
-				k.key <- c
-			}
-			return nil
-		})
-	}
-	return k
-}
-
-func (k *gocuiKeypad) Pressed(key uint8) bool {
-	return k.state[key]
-}
-
-func (k *gocuiKeypad) WaitPress() uint8 {
-	k.wantnext = true
-	v := <-k.key
-	k.wantnext = false
-	return v
-}
-
-type gocuiRenderer struct {
-	*gocui.View
-}
-
-func NewGocuiRenderer(view *gocui.View) *gocuiRenderer {
-	return &gocuiRenderer{view}
-}
-
-func (d *gocuiRenderer) Render(i IterableImage) {
-	i.OnEachPixel(func(x, y int, i WriteableImage) {
-		d.SetCursor(x, y)
-		d.EditDelete(false)
-
-		c := i.At(x, y)
-		if c != 0 {
-			d.EditWrite('\u2588')
-		} else {
-			d.EditWrite(' ')
-		}
-	})
 }
 
 func NewDebugger() *Debugger {
@@ -318,7 +231,7 @@ func (d *Debugger) Start(rom string) {
 
 	d.ui = &ui{g, nil, nil, nil}
 	g.SetManagerFunc(d.ui.layout)
-	// HACK: layout needs to have been called so grab handles to views
+	// HACK: layout needs to have been called to grab handles to views
 	d.ui.layout(g)
 	g.SetCurrentView("display")
 
@@ -358,6 +271,7 @@ func (d *Debugger) Start(rom string) {
 
 func (d *Debugger) run() {
 	go d.c.KeepTime()
+	var tick = time.Tick(2 * time.Millisecond)
 	d.printContext()
 
 	first = true // To allow us to run while on a bp
@@ -404,9 +318,6 @@ LOOP:
 			})
 		}
 	}
-}
-
-func (d *Debugger) PrintAsm(addr, count int) {
 }
 
 func (d *Debugger) printState() {
